fc96a5ef1e1956daa886a29c8e374f3f0a9e2bf8
Salman Ali <salmanafridi945@gmail.com>
2024-06-21 09:06:59 -0700

    Different Star Patterns

--- Lecture5/Main.java
+++ Lecture5/Main.java
@@ -1,37 +1,125 @@
 package Lecture5;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        // pattern 1
+        int n = 5;
+        int m = 4;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
+
+        // pattern 2
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
 
-                System.out.print("* ");
+                System.out.print(i);
 
             }
             System.out.print("\n");
 
         }
+        System.out.println("-----------------");
 
-        // another pattern
-        System.out.print("\n------------\n\n");
-        for (int i = 5; i >= 1; i--) {
+        // pattern 3
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
+
+        // pattern 4
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-
-                System.out.print("* ");
-
+                System.out.print("*");
             }
-            System.out.print("\n");
-
+            System.out.println();
         }
-        System.out.print("\n------------\n");
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 5; j >= i; j--) {
 
-                System.out.print(" * ");
+        System.out.println("-----------------");
 
+        // pattern 5
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
-            System.out.print("\n");
-
+            System.out.println();
         }
+
+        System.out.println("-----------------");
+
+        // pattern 6
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("-----------------");
+
+        // pattern 7
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println("-----------------");
+
+        // pattern 8
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------");
+
+        // pattern 9
+        int number = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+
+        System.out.println("-----------------");
+
+        // pattern 10
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("-----------------");
+
     }
 }
