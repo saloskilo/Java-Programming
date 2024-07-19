905bf943d952f19ca68c88ae572388b04ff3dd39
Salman Ali <salmanafridi945@gmail.com>
2024-07-19 23:30:45 +0500

    print linked list

--- Lecture18/LL.java
+++ Lecture18/LL.java
@@ -2,7 +2,8 @@
 package Lecture18;
 
 public class LL {
-Node head;
+    
+
     class Node {
         String data;
         Node next;
@@ -12,7 +13,8 @@ Node head;
             this.next = null;
         }
     }
-// add in the start of linkedlist 
+    Node head;
+    // add in the start of linkedlist
     public void addFirst(String data) {
 
         Node newNode = new Node(data);
@@ -24,25 +26,43 @@ Node head;
         head = newNode;
     }
 
-// add in the ending of linkedlist 
-public void addLast(String data) {
+    // add in the ending of linkedlist
+    public void addLast(String data) {
 
-    Node newNode = new Node(data);
-    if (head == null) {
-        head = newNode;
-        return;
-    }
-    Node currentNode=head;
-    while (currentNode.next!=null) {
-        currentNode=currentNode.next;
-    }
-    currentNode=newNode;
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
 
-}
+    }
+
+    // print list 
+    public void print(){
+
+        if (head == null) {
+           System.out.println("list is emty");
+            return;
+        }
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            System.out.println(currentNode.data+" - >");
+            currentNode = currentNode.next;
+        }
+        System.out.println("null");
+
+    }
+
     public static void main(String[] args) {
         LL list = new LL();
 
-
         list.addFirst("is");
+
+        list.print();
     }
 }
