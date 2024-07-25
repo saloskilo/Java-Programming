a75e94faeb7ac8ac469248aa19d78e01124372c3
Salman Ali <salmanafridi945@gmail.com>
2024-07-25 21:34:44 +0500

    lec18

---
+++ Lecture18/LiL.java
@@ -0,0 +1,22 @@
+package Lecture18;
+
+public class LiL {
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head;
+
+
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
