da045d5593b166181b6d11ed6bbb8b24e828257f
Salman Ali <143355317+saloskilo@users.noreply.github.com>
2024-09-08 23:55:23 +0500

    InsertionSort

--- DSA PART/Sorting/src/App.java
+++ DSA PART/Sorting/src/App.java
@@ -1,7 +1,26 @@
- 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
     }
-}
+ 
+    public static void main(String[] args) {
+        int arr[] = { 4, 3, 1, 5, 7 };
         
+            // Selection Sort 
+            for (int i = 0; i < arr.length-1; i++) {
+                int smallest =i;
+                for (int j = i+1; j < arr.length; j++) {
+                    if(arr[smallest]>arr[j]){
+                        smallest=j;
+                    }
+                    int temp=arr[smallest];
+                    arr[smallest]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        printArray(arr);
+    }
+} 
